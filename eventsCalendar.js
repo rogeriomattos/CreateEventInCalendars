9e0e89fb1d7f9333b58627228bda1a61eba43cd6
rogerio.mattos7 <rogeriomattos38@outlook.com>
2019-06-11 11:51:37 -0300

    abstraindo para um arquivo apenas

--- eventsCalendar.js
+++ eventsCalendar.js
@@ -2,14 +2,50 @@ var eventCalendar = {
     openCalendar : {
         
         google : function(event){
-            var query = "http://www.google.com/calendar/event?"+
+            var url = "http://www.google.com/calendar/event?"+
                     "action=TEMPLATE"+
                     "&text="+event.summary+
                     "&dates="+event.startDateTime+"/"+event.endDateTime+
                     "&details="+event.body+
                     "&location="+event.location;
-            window.open(query, "_blank");
-        }
+            window.open(url, "_blank");
+        },
+        outlook365 : function(event){
+            var url =  "https://outlook.office.com/owa//?path=/calendar/action/compose&rru=addevent"+
+                        "&startdt="+ event.startDateTime +
+                        "&enddt="+ event.endDateTime +
+                        "&subject="+ event.summary +
+                        "&location="+ event.location + 
+                        "&body="+ event.body +
+                        "&allday="+event.allDay;
+            
+            window.open(url, "_blank");
+        },
+        ics : function(event){
+            var query = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//PT\n"+
+            "BEGIN:VEVENT\nUID:"+
+                "\nDTSTAMP:"+event.today+
+                "\nATTENDEE;CN=My Self ;RSVP=TRUE"+
+                "\nORGANIZER;CN=Me"+
+                "\nDTSTART:" + event.startDateTime +
+                "\nDTEND:" + event.endDateTime +
+                "\nLOCATION:" + event.location + 
+                "\nSUMMARY:"+ event.summary +
+                "\nBODY:"+ event.body +
+            "\nEND:VEVENT"+
+            "\nEND:VCALENDAR";
+
+            var element = document.createElement('a');
+            element.setAttribute('href', "data:text/calendar;charset=utf8,"+escape(query));
+            element.setAttribute('download', "Evento");
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+        } 
+
     }
 };
-console.log(eventCalendar);
--- script.js
+++ script.js
@@ -4,22 +4,16 @@ $( document ).ready(function() {
   
 
     $('#outlook365').click(function(){
-        var startDateTime = new Date($('#start-time').val()).toISOString();    
-        var endDateTime = new Date($('#end-time').val()).toISOString();
-        var location = $('#location').val();
-        var summary = $('#summary').val();
-        var body = $("#body").val();
-        var allDay = $("#allDay").is(':checked');
-        
-        var query = "https://outlook.office.com/owa//?path=/calendar/action/compose&rru=addevent"+
-                    "&startdt="+ startDateTime +
-                    "&enddt="+ endDateTime +
-                    "&subject="+ summary +
-                    "&location="+ location + 
-                    "&body="+ body +
-                    "&allday="+allDay;
+        var event ={
+            startDateTime : new Date($('#start-time').val()).toISOString(),    
+            endDateTime : new Date($('#end-time').val()).toISOString(),
+            location : $('#location').val(),
+            summary : $('#summary').val(),
+            body : $("#body").val(),
+            allDay : $("#allDay").is(':checked'),
+        };
 
-        window.open(query, "_blank");
+        eventCalendar.openCalendar.outlook365(event);
     });
 
     $("#google").click(function(){
@@ -46,37 +40,17 @@ $( document ).ready(function() {
     });
 
     $("#fileIcs").click(function(){
-        var startDateTime = new Date($('#start-time').val()).toISOString();    
-        var endDateTime = new Date($('#end-time').val()).toISOString();
-        var location = $('#location').val();
-        var summary = $('#summary').val();
-        var body = $("#body").val();
-        var allDay = $("#allDay").is(':checked');
-        var today = new Date().toISOString();
-        var query = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//PT\n"+
-                    "BEGIN:VEVENT\nUID:"+
-                        "\nDTSTAMP:"+today+
-                        "\nATTENDEE;CN=My Self ;RSVP=TRUE"+
-                        "\nORGANIZER;CN=Me"+
-                        "\nDTSTART:" + startDateTime +
-                        "\nDTEND:" + endDateTime +
-                        "\nLOCATION:" + location + 
-                        "\nSUMMARY:"+ summary +
-                        "\nBODY:"+ body +
-                    "\nEND:VEVENT"+
-                    "\nEND:VCALENDAR";
-
-        var element = document.createElement('a');
-        element.setAttribute('href', "data:text/calendar;charset=utf8,"+escape(query));
-        element.setAttribute('download', "Evento");
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
         
-        //window.open();
+        var event ={
+            startDateTime : new Date($('#start-time').val()).toISOString(),    
+            endDateTime : new Date($('#end-time').val()).toISOString(),
+            location : $('#location').val(),
+            summary : $('#summary').val(),
+            body : $("#body").val(),
+            allDay : $("#allDay").is(':checked'),
+            today : new Date().toISOString(),
+        };
+
+        eventCalendar.openCalendar.ics(event);
     });
 });
